ecf1cce4c2b7d482b8bfb9c2300235bb8741ea77
Akram <akrambey.t@gmail.com>
2020-11-17 16:50:06 +0500

    default save

---
+++ script/script1.js
@@ -0,0 +1,52 @@
+//Массивы
+//Упражнения
+//#1 - Дразнилка
+/*
+var randomBodyParts   = ['глаза','руки','ноги','ногти','уши','палцы','волосы'];
+var randomAdjectives = ['смешные','необычные','красивые','точ в точ','стрёмные','смешные'];
+var randomAnimals    = ['мишки','лва','тигра','обезяны','макаки','медведя'];
+
+var randomAnimal     = randomAnimals[Math.floor(Math.random()*randomAnimals.length)];
+var randomAdjective  = randomAdjectives[Math.floor(Math.random()*randomAdjectives.length)];
+var randomBodyPart   = randomBodyParts[Math.floor(Math.random()*randomBodyParts.length)];
+//1-способ
+var randomInsult     = 'Твои '+ randomBodyPart + ' такие ' + randomAdjective + ' как и у ' + randomAnimal + '!!!';
+//2-второй
+var randomInsult2    = ["Твои" , randomBodyPart ,"такие",randomAdjective,"как и у",randomAnimal,"!!!"].join(' ');
+alert(randomInsult);
+*/
+//#2-Подсчёт очков
+
+// var scores = {
+//     team1 : 0,
+//     team2 : 0
+// };
+// scores['team1'] += 5;
+
+//Practice Objects
+/*
+var myCrazyObject = {
+    name: "Какой-то объект",
+    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
+    "random animal": "Черепаха"
+    };
+    var needNumber = myCrazyObject["some array"][2];
+    var my123 = needNumber.number;
+
+    console.log(my123);
+    
+    
+    
+    var anna = {name: 'Анна',age: 23,luckuNumbers:[3,5,9]};
+    var dave = {name: 'Дейв',age: 15,luckyNumbers:[2,4,6]};
+    var sara = {name: 'Сара',age: 15,luckyNumbers:[1,5,7]};
+    var friends = [anna,dave,sara];
+*/
+
+/*
+var animals = ['лев','зебра','слон','тигр','рыбы'];
+//var animal = ['У нас в зоопарке есть',animals,'!'].join(' ');
+for (var i=0;i<animals.length;i++){
+    console.log("В этом зоопарке есть " + animals[i] + ".");
+}
+*/
